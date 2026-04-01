81c664a9d0a460f885d50a124b07952e68abed8f
Hamid Ali <hamidalig942@gmail.com>
2026-04-01 11:29:21 +0500

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: 'Roll Dice',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: Scaffold(body: Center(child: const Text('Hello World!'))),
     ),
   );
